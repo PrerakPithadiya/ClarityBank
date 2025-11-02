b37cda6b6fffc3e52f83b151f736589318bfbe3d
Prerak Apple <prerakapple@gmail.com>
2025-11-02 10:24:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4772614567-604ea",
+  "appId": "1:1066045412597:web:11162ace5a1ed3e35361bc",
+  "apiKey": "AIzaSyBk6kk4P9s46fKp3T7o5GF_x9tNY5t6SFw",
+  "authDomain": "studio-4772614567-604ea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1066045412597"
+};
