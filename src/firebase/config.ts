4290010220793876ce8c13982985b86ef58796ed
Prerak Apple <prerakapple@gmail.com>
2025-11-20 10:42:48 +0000

    Fix this issue

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-4772614567-604ea",
   "appId": "1:1066045412597:web:11162ace5a1ed3e35361bc",
-  "apiKey": "AIzaSyBk6kk4P9s46fKp3T7o5GF_x9tNY5t6SFw",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-4772614567-604ea.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1066045412597"
